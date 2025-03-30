577813a8d083c77eb5c3734b559f261ce3914408
Fabius-Sever Pop <132004909+fabiusspop@users.noreply.github.com>
2025-03-30 12:23:20 +0300

    Added models for Books
    - Theme
    - Reading level
    - Book class containing title, themes, reading_level

--- book_recommender/books/models.py
+++ book_recommender/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Theme(models.Model):
+    name = models.CharField(max_length = 100, unique = True)
+    
+    def __str__(self):
+        return self.name
+    
+class ReadingLevel(models.Model):
+    name = models.CharField(max_length = 100, unique = True)
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length = 200)
+    themes = models.ManyToManyField(Theme, related_name = 'books', limit_choices_to = 2)
+    reading_level = models.ManyToManyField(ReadingLevel, related_name = 'books')
+    
+    def __str__(self):
+        return self.title
